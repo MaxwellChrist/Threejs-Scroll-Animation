f91668be1612cd5cb4c3b3095ab985db47615edd
MaxwellChrist <ribthulu@gmail.com>
2022-07-04 13:51:20 -0400

    took care of scrolling feature and bug to see background, created three objects and rotated them

--- src/script.js
+++ src/script.js
@@ -8,11 +8,10 @@ import * as dat from 'lil-gui'
 const gui = new dat.GUI()
 
 const parameters = {
-    materialColor: '#ffeded'
+    materialColor: '#440D0F'
 }
 
-gui
-    .addColor(parameters, 'materialColor')
+gui.addColor(parameters, 'materialColor').onChange(() => material.color.set(parameters.materialColor))
 
 /**
  * Base
@@ -24,13 +23,49 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Test cube
+ * Texture
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const textureLoader = new THREE.TextureLoader()
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
+
+/**
+ * Objects
+ */
+const material = new THREE.MeshToonMaterial({ 
+    color: parameters.materialColor,
+    gradientMap: gradientTexture
+})
+
+// Meshes
+const objectsDistance = 3.5
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16),
+    material
 )
-scene.add(cube)
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 0.4, 16),
+    material
+)
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(1, 0.4, 16),
+    material
+)
+
+mesh1.position.y = -(objectsDistance * 0)
+mesh2.position.y = -(objectsDistance * 1)
+mesh3.position.y = -(objectsDistance * 2)
+
+scene.add(mesh1, mesh2, mesh3)
+
+const sectionMeshes = [mesh1, mesh2, mesh3]
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.position.set(1, 1, 0)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -67,7 +102,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true // allows you to see through you canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -80,6 +116,12 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animate meshes
+    for (let mesh of sectionMeshes) {
+        mesh.rotation.x = elapsedTime * 0.25
+        mesh.rotation.y = elapsedTime * 0.25
+    }
 
     // Render
     renderer.render(scene, camera)
